794f0ee562b844536ac4fb2c189c02a3e54ba1e9
beihu235 <2120024137@qq.com>
2025-10-19 17:13:47 +0800

    调整下配额试试

--- src/hx/gc/GcCommon.cpp
+++ src/hx/gc/GcCommon.cpp
@@ -34,7 +34,7 @@ int sgMinimumWorkingMemory       = 384*1024*1024;
 int sgMinimumFreeSpace           = 64*1024*1024;
 #endif
 // Once you use more than the minimum, this kicks in...
-int sgTargetFreeSpacePercentage  = 80;
+int sgTargetFreeSpacePercentage  = 10;
 
 
 
